1630683716cee7f7b46d414ec52cc7a4caf21c5e
JimmyTorres <jimmy@imaginefour.com>
2022-06-18 12:59:11 -0500

    Feat: Se inicializa el proyecto

---
+++ src/main/java/com/imagine/gestionsoft/core/GestionsoftCoreApplication.java
@@ -0,0 +1,13 @@
+package com.imagine.gestionsoft.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionsoftCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionsoftCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imagine/gestionsoft/core/GestionsoftCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imagine.gestionsoft.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionsoftCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
